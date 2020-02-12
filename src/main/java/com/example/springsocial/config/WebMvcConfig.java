df9b83b7703f883ee4b6b2d5cad2dd0ab1c3daa2
juney <juneyj114@gmail.com>
2020-02-12 17:34:14 +0900

    0212 debug

--- src/main/java/com/example/springsocial/config/WebMvcConfig.java
+++ src/main/java/com/example/springsocial/config/WebMvcConfig.java
@@ -1,10 +1,8 @@
 package com.example.springsocial.config;
 
-import org.springframework.boot.web.servlet.view.MustacheViewResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
